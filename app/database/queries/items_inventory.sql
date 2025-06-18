839a4aec921981031404ee94b6c88b4c86a16d95
josh goble <joshgoble@gmail.com>
2025-06-17 21:06:03 -0500

    Deploy: automated deployment at 2025-06-17 21:06:03

--- app/database/queries/items_inventory.sql
+++ app/database/queries/items_inventory.sql
@@ -20,10 +20,14 @@ SELECT
     COALESCE(sile.default_vendor_name, '') AS vendor_name,
     COALESCE(sile.default_vendor_code, '') AS vendor_code,
     
-    -- Cost information - use square_item_library_export only (catalog_variations may not have default_unit_cost)
-    sile.default_unit_cost AS cost,
+    -- Cost information - try both sources with proper fallback
+    CASE 
+        WHEN cv.default_unit_cost IS NOT NULL AND (cv.default_unit_cost->>'amount') IS NOT NULL AND (cv.default_unit_cost->>'amount') != ''
+        THEN (cv.default_unit_cost->>'amount')::numeric / 100
+        ELSE sile.default_unit_cost
+    END AS cost,
     
-    -- Profit margin calculation
+    -- Profit margin calculation: (Price - Cost) / Price * 100
     CASE 
         WHEN (
             CASE 
@@ -32,7 +36,13 @@ SELECT
                 ELSE sile.price
             END
         ) IS NOT NULL 
-        AND sile.default_unit_cost IS NOT NULL 
+        AND (
+            CASE 
+                WHEN cv.default_unit_cost IS NOT NULL AND (cv.default_unit_cost->>'amount') IS NOT NULL AND (cv.default_unit_cost->>'amount') != ''
+                THEN (cv.default_unit_cost->>'amount')::numeric / 100
+                ELSE sile.default_unit_cost
+            END
+        ) IS NOT NULL 
         AND (
             CASE 
                 WHEN cv.price_money IS NOT NULL AND (cv.price_money->>'amount') IS NOT NULL AND (cv.price_money->>'amount') != ''
@@ -45,7 +55,12 @@ SELECT
                 WHEN cv.price_money IS NOT NULL AND (cv.price_money->>'amount') IS NOT NULL AND (cv.price_money->>'amount') != ''
                 THEN (cv.price_money->>'amount')::numeric / 100
                 ELSE sile.price
-            END) - sile.default_unit_cost
+            END) - 
+            (CASE 
+                WHEN cv.default_unit_cost IS NOT NULL AND (cv.default_unit_cost->>'amount') IS NOT NULL AND (cv.default_unit_cost->>'amount') != ''
+                THEN (cv.default_unit_cost->>'amount')::numeric / 100
+                ELSE sile.default_unit_cost
+            END)
         ) / (
             CASE 
                 WHEN cv.price_money IS NOT NULL AND (cv.price_money->>'amount') IS NOT NULL AND (cv.price_money->>'amount') != ''
@@ -65,15 +80,38 @@ SELECT
                 ELSE sile.price
             END
         ) IS NOT NULL 
-        AND sile.default_unit_cost IS NOT NULL 
-        AND sile.default_unit_cost > 0
+        AND (
+            CASE 
+                WHEN cv.default_unit_cost IS NOT NULL AND (cv.default_unit_cost->>'amount') IS NOT NULL AND (cv.default_unit_cost->>'amount') != ''
+                THEN (cv.default_unit_cost->>'amount')::numeric / 100
+                ELSE sile.default_unit_cost
+            END
+        ) IS NOT NULL 
+        AND (
+            CASE 
+                WHEN cv.default_unit_cost IS NOT NULL AND (cv.default_unit_cost->>'amount') IS NOT NULL AND (cv.default_unit_cost->>'amount') != ''
+                THEN (cv.default_unit_cost->>'amount')::numeric / 100
+                ELSE sile.default_unit_cost
+            END
+        ) > 0
         THEN ROUND(((
             (CASE 
                 WHEN cv.price_money IS NOT NULL AND (cv.price_money->>'amount') IS NOT NULL AND (cv.price_money->>'amount') != ''
                 THEN (cv.price_money->>'amount')::numeric / 100
                 ELSE sile.price
-            END) - sile.default_unit_cost
-        ) / sile.default_unit_cost * 100)::numeric, 2)
+            END) - 
+            (CASE 
+                WHEN cv.default_unit_cost IS NOT NULL AND (cv.default_unit_cost->>'amount') IS NOT NULL AND (cv.default_unit_cost->>'amount') != ''
+                THEN (cv.default_unit_cost->>'amount')::numeric / 100
+                ELSE sile.default_unit_cost
+            END)
+        ) / (
+            CASE 
+                WHEN cv.default_unit_cost IS NOT NULL AND (cv.default_unit_cost->>'amount') IS NOT NULL AND (cv.default_unit_cost->>'amount') != ''
+                THEN (cv.default_unit_cost->>'amount')::numeric / 100
+                ELSE sile.default_unit_cost
+            END
+        ) * 100)::numeric, 2)
         ELSE NULL 
     END AS profit_markup_percent,
     
@@ -87,14 +125,42 @@ SELECT
     0 AS terrell_qty,
     0 AS total_qty,
     
-    -- Location availability (from JSON data)
-    CASE WHEN sile.location_data->'Aubrey'->>'enabled' = 'true' THEN 'Yes' ELSE 'No' END AS aubrey_enabled,
-    CASE WHEN sile.location_data->'Bridgefarmer'->>'enabled' = 'true' THEN 'Yes' ELSE 'No' END AS bridgefarmer_enabled,
-    CASE WHEN sile.location_data->'Building'->>'enabled' = 'true' THEN 'Yes' ELSE 'No' END AS building_enabled,
-    CASE WHEN sile.location_data->'FloMo'->>'enabled' = 'true' THEN 'Yes' ELSE 'No' END AS flomo_enabled,
-    CASE WHEN sile.location_data->'Justin'->>'enabled' = 'true' THEN 'Yes' ELSE 'No' END AS justin_enabled,
-    CASE WHEN sile.location_data->'Quinlan'->>'enabled' = 'true' THEN 'Yes' ELSE 'No' END AS quinlan_enabled,
-    CASE WHEN sile.location_data->'Terrell'->>'enabled' = 'true' THEN 'Yes' ELSE 'No' END AS terrell_enabled,
+    -- Location availability - safe approach with existence checks
+    CASE 
+        WHEN sile.enabled_aubrey = 'Y' THEN 'Yes'
+        WHEN sile.enabled_aubrey = 'N' THEN 'No'
+        ELSE 'No'
+    END AS aubrey_enabled,
+    CASE 
+        WHEN sile.enabled_bridgefarmer = 'Y' THEN 'Yes'
+        WHEN sile.enabled_bridgefarmer = 'N' THEN 'No'
+        ELSE 'No'
+    END AS bridgefarmer_enabled,
+    CASE 
+        WHEN sile.enabled_building = 'Y' THEN 'Yes'
+        WHEN sile.enabled_building = 'N' THEN 'No'
+        ELSE 'No'
+    END AS building_enabled,
+    CASE 
+        WHEN sile.enabled_flomo = 'Y' THEN 'Yes'
+        WHEN sile.enabled_flomo = 'N' THEN 'No'
+        ELSE 'No'
+    END AS flomo_enabled,
+    CASE 
+        WHEN sile.enabled_justin = 'Y' THEN 'Yes'
+        WHEN sile.enabled_justin = 'N' THEN 'No'
+        ELSE 'No'
+    END AS justin_enabled,
+    CASE 
+        WHEN sile.enabled_quinlan = 'Y' THEN 'Yes'
+        WHEN sile.enabled_quinlan = 'N' THEN 'No'
+        ELSE 'No'
+    END AS quinlan_enabled,
+    CASE 
+        WHEN sile.enabled_terrell = 'Y' THEN 'Yes'
+        WHEN sile.enabled_terrell = 'N' THEN 'No'
+        ELSE 'No'
+    END AS terrell_enabled,
     
     -- Additional item attributes
     COALESCE(sile.item_type, 'REGULAR') as item_type,
@@ -115,12 +181,12 @@ ORDER BY sile.item_name ASC
 
 -- Note: This query provides comprehensive item data including:
 -- 1. All basic item information (name, SKU, description, categories)
--- 2. Complete pricing data (price, unit cost, profit margin) - prioritizing catalog_variations for price, square_item_library_export for cost
+-- 2. Complete pricing data (price, unit cost, profit margin) - prioritizing catalog_variations over square_item_library_export
 -- 3. Full vendor information (name, code)
 -- 4. Item attributes (sellable, stockable, archived status)
 -- 5. Location availability flags
 -- 6. Metadata timestamps
 --
--- Data Source: Uses catalog_variations for price data and square_item_library_export for cost data
--- since not all environments have the default_unit_cost column in catalog_variations.
--- Inventory quantities are set to 0 for now to avoid complex joins. 
+-- Data Source: Prioritizes catalog_variations table for cost/price data since that contains
+-- the most current information from Square's API, falling back to square_item_library_export
+-- for any missing data. Inventory quantities are set to 0 for now to avoid complex joins. 
